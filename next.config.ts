cd39c267bbe255db1fc3246cac7cb41e6b7ab30d
Varad <120244476+Varad-02@users.noreply.github.com>
2026-08-04 02:36:04 +0530

    Implemented database setup

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {remotePatterns: [
-          { protocol: 'https', hostname: 'covers.openlibrary.org' }
+          { protocol: 'https', hostname: 'covers.openlibrary.org' },
+          { protocol: 'https', hostname: '0hewkr0pb8b4qubb.public.blob.vercel-storage.com' }
       ]}
 };
 
